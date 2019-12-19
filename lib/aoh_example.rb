2eaedbd5dd8a61212f13bf3bdc856d515f5ce560
lior Shine <shinelior@gmail.com>
2019-12-19 16:39:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,9 +15,9 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-new_has={}
-  assembled_aoh.sort_by(name:,occupation:)
+
   
+  retun=assembled_aoh.map(&:sort).map(&:to_h)
 end
 
 def aoh_lookup(aoh, row, key)
